aaf61cf20964e47892474cccbe3de91bb3add75c
roi biton <roibitonroi@gmail.com>
2025-11-13 22:12:38 +0200

    Add main screen overlay and toggle menu functionality with responsive design updates

--- js/main.js
+++ js/main.js
@@ -6,13 +6,23 @@ function onInit() {
 
 function onShowGallery() {
     showGallery()
-
+    closeMenu()
 }
 
 function onShowEditor() {
     showEditor()
+    closeMenu()
 }
 
 function onShowSavedMemes() {
     showSavedMemes()
+    closeMenu()
+}
+
+function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-open');
 }
